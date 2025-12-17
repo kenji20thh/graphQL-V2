e5f330e180eec440b33dd47c6bfc481d29f97576
kenji20thh <amineelayachi22@outlook.com>
2025-12-17 17:21:50 +0100

    config fixed

--- graphql-profile/js/config.js
+++ graphql-profile/js/config.js
@@ -1,2 +1,9 @@
-export const GRAPHQL_ENDPOINT = '/graphql';
+// API Endpoints
+export const GRAPHQL_ENDPOINT = 'https://learn.zone01oujda.ma/api/graphql-engine/v1/graphql';
+export const SIGNIN_ENDPOINT = 'https://learn.zone01oujda.ma/api/auth/signin';
+
+// App Configuration
 export const APP_TITLE = 'GraphQL Profile';
+
+// GraphQL Settings
+export const GRAPHQL_TIMEOUT = 10000; // 10 seconds
