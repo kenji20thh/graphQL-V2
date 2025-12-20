768b9a13563e63364e60fd3cf1d7ea1520d2d304
kenji20thh <amineelayachi22@outlook.com>
2025-12-20 16:38:41 +0100

    updated header

--- graphql-profile/js/components/header.js
+++ graphql-profile/js/components/header.js
@@ -1,9 +1,15 @@
 import { removeToken } from '../auth/token-manager.js';
 
-export function createHeader(name){
+export function createHeader(name) {
   const el = document.createElement('div');
   el.className = 'profile-header card';
-  el.innerHTML = `<div><strong>${name}</strong></div><div><button id="logout" class="btn">Logout</button></div>`;
-  el.querySelector('#logout').addEventListener('click', ()=>{ removeToken(); window.location.reload(); });
+  el.innerHTML = `
+    <div><strong>${name}</strong></div>
+    <div><button id="logout" class="btn">Logout</button></div>
+  `;
+  el.querySelector('#logout').addEventListener('click', () => { 
+    removeToken(); 
+    window.location.reload(); 
+  });
   return el;
 }
